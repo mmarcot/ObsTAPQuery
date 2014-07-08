de032bb87e74d54ea7bc0b02de57769c901bb1db
Mallory MARCOT <mmarcot@gmail.com>
2014-07-08 17:30:48 +0200

    Ajout d'une tolérance de nettoyage lors du changement de table

--- src/listeners/AuditeurComboBoxFrom.java
+++ src/listeners/AuditeurComboBoxFrom.java
@@ -19,29 +19,69 @@ import metier.GestionnaireDeTables;
  *
  */
 public class AuditeurComboBoxFrom implements ActionListener{
+	
+	private JComboBox combo_src;
+	private PluginLayout plug_lay;
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		JComboBox combo_src = (JComboBox) e.getSource();
-		PluginLayout plug_lay = (PluginLayout) combo_src.getParent().getParent().getParent();
+		combo_src = (JComboBox) e.getSource();
+		plug_lay = (PluginLayout) combo_src.getParent().getParent().getParent();
 		
-		if(JOptionPane.showConfirmDialog(plug_lay, Langage.getReq_changer_table_message_dialog(), Langage.getAttention(), JOptionPane.YES_NO_CANCEL_OPTION) == JOptionPane.YES_OPTION) {
-			GestionnaireDeTables.changerTableSelectionnee((String) combo_src.getSelectedItem());
-			
-			// on nettoye tout :
-			plug_lay.getSelect().uncheckAll();
-			plug_lay.getWhere().toutEnlever();
-			plug_lay.getRequete().setTexteRequete(null);
-			
-			// on met à jour :
-			plug_lay.getSelect().mettreAJourColonnes();
-			plug_lay.getWhere().getGestionnaire().mettreAJourColonnes();
-		}
-		else { // on remet la JComboBox sur l'item précédent :
-			String table_prec = GestionnaireDeTables.getTableSelectionnee();
-			combo_src.setSelectedItem(table_prec);
-		}
+		// on détermine quand est-ce qu'il faut demander confirmation :
+		boolean demander_confirmation = false;
+		if(plug_lay.getWhere().getGestionnaire().getComponentCount() >= 2)
+			demander_confirmation = true;
 		
+		if(demander_confirmation) {
+			if( JOptionPane.showConfirmDialog(plug_lay, Langage.getReq_changer_table_message_dialog(), Langage.getAttention(), JOptionPane.YES_NO_CANCEL_OPTION) == JOptionPane.YES_OPTION) {
+				mettreAJour();
+			}
+			else { // on remet la JComboBox sur l'item précédent :
+				String table_prec = GestionnaireDeTables.getTableSelectionnee();
+				combo_src.setSelectedItem(table_prec);
+			}
+		}
+		else { // pas de confirmation
+			mettreAJour();
+		}
 	}
 
+	
+	/**
+	 * Permet de tout nettoyer et de mettre à jour les colonnes 
+	 */
+	private void mettreAJour() {
+		GestionnaireDeTables.changerTableSelectionnee((String) combo_src.getSelectedItem());
+		
+		// on nettoye tout :
+		plug_lay.getSelect().uncheckAll();
+		plug_lay.getWhere().toutEnlever();
+		plug_lay.getRequete().setTexteRequete(null);
+		
+		// on met à jour :
+		plug_lay.getSelect().mettreAJourColonnes();
+		plug_lay.getWhere().getGestionnaire().mettreAJourColonnes();
+	}
+	
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
